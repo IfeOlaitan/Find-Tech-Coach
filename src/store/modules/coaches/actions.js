073fc523b93ad9f75461dd61b7df14bcfb1b3d97
Ifeoluwa Olaitan <ifeoluwa1201@gmail.com>
2022-08-21 14:23:25 +0100

    put http request

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,7 +1,8 @@
 export default {
-    registerCoachData(context, formData) {
+    async registerCoachData(context, formData) {
+        const userId = context.rootGetters.userId;
+
         const coachData = {
-            id: context.rootGetters.userId,
             firstName: formData.first,
             lastName: formData.last,
             areas: formData.areas,
@@ -9,6 +10,21 @@ export default {
             hourlyRate: formData.rate,
             img: formData.img
         };
-        context.commit('addNewCoach', coachData);
+
+        const response = await fetch(`https://find-test-coach-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+            method: 'PUT',
+            body: JSON.stringify(coachData)
+        });
+
+        // const responseData = await response.json();
+
+        if (!response.ok) {
+            //error
+        }
+
+        context.commit('addNewCoach', {
+            ...coachData,
+            id: userId
+        });
     }
 }
